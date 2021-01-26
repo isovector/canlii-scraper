1be4361c6220117b0afebb2e35994fdcc1f4b2f7
Sandy Maguire <sandy@sandymaguire.me>
2021-01-26 15:04:42 -0800

    Backfill decade and decade rank

--- backfill.py
+++ backfill.py
@@ -9,6 +9,8 @@ def alter(conn):
     conn.execute('alter table decisions add column jurisdiction CHAR(2);')
     conn.execute('alter table decisions add column court CHAR(8);')
     conn.execute('alter table decisions add column year INT;')
+    conn.execute('alter table decisions add column decade INT;')
+    conn.execute('alter table decisions add column decade_rank INT;')
     conn.commit()
 
 
@@ -24,10 +26,30 @@ def backfill(conn):
         jurisdiction = bits[2]
         court = bits[3]
         year = bits[5]
+        decade = int(year) / 10 * 10
 
-        db.execute('update decisions set jurisdiction = ?, court = ?, year = ? where hash = ?', (jurisdiction, court, year, row['hash']));
+        db.execute('update decisions set jurisdiction = ?, court = ?, year = ?, decade = ? where hash = ?', (jurisdiction, court, year, decade, row['hash']));
 
         print (jurisdiction, court, year)
 
     conn.commit()
+
+def cleanup(conn):
+    db = conn.cursor()
+    db.execute('update decisions set year = 1951, decade = 1950 where hash = "ada4c5ad7435e34298b5a14290227ccab5bc67fc"')
+    conn.commit()
+
+
+def normalize_decade(conn):
+    q = conn.cursor()
+    q.execute('select decade from decisions order by decade asc limit 1;')
+    smallest = q.fetchone()
+    q.execute('update decisions set decade_rank = (decade - ?) / 10', (smallest['decade'],))
+    conn.commit()
+
+
+# alter(conn)
+# backfill(conn)
+cleanup(conn)
+normalize_decade(conn)
 
