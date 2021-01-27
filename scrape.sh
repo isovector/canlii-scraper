064ec58421552a46ba83d5fa7df17a1aeadaeac0
Sandy Maguire <sandy@sandymaguire.me>
2021-01-26 21:58:50 -0800

    scrape.sh to orchestrate the whole thing

--- scrape.sh
+++ scrape.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-java -jar selenium.jar &
 
 while :; do
+  java -jar selenium.jar &
   mullvad relay set relay `cat relays| shuf | head -n1`
-  sleep 5s
+  sleep 20s
   stack run &
   sleep 2s
   python2 canlii.py
   pkill canlii
+  pkill java
 done
 
 
