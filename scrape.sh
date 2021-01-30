63377a2cc793451438063eef9c9ca2dee7c7ea37
Sandy Maguire <sandy@sandymaguire.me>
2021-01-29 16:18:09 -0800

    Cleanup RAM

--- scrape.sh
+++ scrape.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-
 while :; do
+  rm -rf /tmp/*
+  wmctrl -c "Firefox"
+  pkill canlii
+  pkill java
+  pkill gecko
   java -jar selenium.jar &
   mullvad relay set relay `cat relays| shuf | head -n1`
   sleep 20s
   stack run &
   sleep 2s
   python2 canlii.py
-  wmctrl -c "| CanLII"
-  pkill canlii
-  pkill java
 done
 
 
